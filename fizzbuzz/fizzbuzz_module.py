8b8f2b335fbe12eadc3333a06a663fe8559c8a58
Greg Thomson <gregthomson@outlook.com>
2022-03-15 09:55:44 +0000

    cleaning up spacing

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -5,11 +5,11 @@ n = int(sys.argv[1])
 
 # Defining the FizzBuzz Function
 def fizzBuzz(n):
-   if n% 3 == 0 and n%5 == 0:
+   if n % 3 == 0 and n % 5 == 0:
       print(str("FizzBuzz"))
-   elif n %3 == 0:
+   elif n % 3 == 0:
       print(str("Fizz"))
-   elif n% 5 == 0:
+   elif n % 5 == 0:
       print(str("Buzz"))
    else:
       print(str(n))
